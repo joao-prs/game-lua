9bec12b92c854ab0a6ed043e6bfb43da1d6a98a0
Joao in Perola <kandrocmatunein@gmail.com>
2024-02-20 10:40:02 -0300

    resolution

--- game/main.lua
+++ game/main.lua
@@ -1,22 +1,14 @@
-
-
-
+-- main
 function love.load()
     love.window.setTitle("Prototipo 1")
     love.window.setFullscreen(false, "exclusive")
-    love.window.width = 800
-    love.window.height = 600
-    love.window.setVSync(2)
-    love.window.msaa = 0
-    love.window.resizable = true
-    love.window.minwidth = 1
-    love.window.minheight = 1
-    love.window.borderless = false
-    love.window.centered = true
-    love.window.display = 1
-    love.window.highdpi = false
-    love.window.x = nil
-    love.window.y = nil
+    
+    -- Define a resolução da janela
+    love.window.setMode(1200, 800, {
+        resizable = true,  -- Permite redimensionar a janela
+        minwidth = 400,    -- Largura mínima da janela
+        minheight = 300    -- Altura mínima da janela
+    })
     
     -- Carrega o arquivo de áudio
     audio = love.audio.newSource("song/AdhesiveWombat_Distortotron.mp3", "stream")
@@ -32,29 +24,19 @@ function love.load()
     }
 end
 
-
-
-function love.quit()
-    -- Para a reprodução do áudio quando o programa é encerrado
-    love.audio.stop()
-end
-
-
 function love.draw()
     love.graphics.setColor(square.color) -- Define a cor para o quadrado
     love.graphics.rectangle("fill", square.x, square.y, square.size, square.size) -- Desenha um retângulo preenchido (quadrado)
 end
 
 
-
-
-
-
-
-
-
 local fullscreen = require("fullscreen")
-
 function love.keypressed(key, scancode, isrepeat)
     fullscreen.love_keypressed(key, scancode, isrepeat)
+end
+
+
+-- finalizar
+function love.quit()
+    love.audio.stop()
 end
