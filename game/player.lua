95cbe5f2f91c37f8e602ed4cf2fe79c5eca53c5e
Joao in Perola <kandrocmatunein@gmail.com>
2024-02-21 10:30:59 -0300

    player sprite add

---
+++ game/player.lua
@@ -0,0 +1,79 @@
+-- Player.lua
+local Player = {}
+
+
+local x, y = 100, 100
+local speed = 200
+local diagonalSpeed = 0.7 -- multiplica a velocidade por valor inferior a 1
+local playerSprite
+local frameWidth, frameHeight = 48, 48 -- Tamanho de cada quadro do sprite
+local framesPerSecond = 6 -- Número de quadros por segundo
+local frameTimer = 0
+local currentFrame = 1
+
+
+local function isOppositeDirection(dir1, dir2)
+    return (dir1 == "up" and dir2 == "down") or
+           (dir1 == "down" and dir2 == "up") or
+           (dir1 == "left" and dir2 == "right") or
+           (dir1 == "right" and dir2 == "left")
+end
+
+
+function Player.load()
+    -- Não é necessário carregar nada neste exemplo
+    playerSprite = love.graphics.newImage("sprite/player.png")
+end
+
+function Player.update(dt)
+    local dx, dy = 0, 0
+    
+    if love.keyboard.isDown("up") then
+        dy = dy - 1
+    elseif love.keyboard.isDown("down") then
+        dy = dy + 1
+    end
+
+    if love.keyboard.isDown("left") then
+        dx = dx - 1
+    elseif love.keyboard.isDown("right") then
+        dx = dx + 1
+    end
+
+    if dx ~= 0 or dy ~= 0 then
+        -- Atualiza a animação do sprite apenas se estiver se movendo
+        frameTimer = frameTimer + dt
+        if frameTimer >= 1 / framesPerSecond then
+            currentFrame = currentFrame % 6 + 1
+            frameTimer = 0
+        end
+    else
+        -- Reseta a animação se o sprite não estiver se movendo
+        currentFrame = 1
+    end
+    if dx ~= 0 and dy ~= 0 then
+        -- Reduz a velocidade quando se move nas diagonais
+        dx = dx * diagonalSpeed
+        dy = dy * diagonalSpeed
+  
+    end
+
+    x = x + dx * speed * dt
+    y = y + dy * speed * dt
+end
+
+function Player.draw()
+    --love.graphics.rectangle("fill", x, y, 50, 50)
+    --love.graphics.draw(playerSprite, x, y)
+
+    local quad = love.graphics.newQuad((currentFrame - 1) * frameWidth, 0, frameWidth, frameHeight, playerSprite:getDimensions())
+    love.graphics.draw(playerSprite, quad, x, y)
+end
+
+
+
+function Player.keypressed(key)
+    -- Você pode adicionar manipulação de teclas adicionais aqui, se necessário
+end
+
+return Player
